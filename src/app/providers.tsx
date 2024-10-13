dc7aa21f43aa5f11826af590536020e33bbf9a22
devmcje <devmcje@gmail.com>
2024-10-13 15:34:24 +0800

    setup: shadcn

---
+++ src/app/providers.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from "@/providers/theme-provider";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Providers({ children }: Props) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
