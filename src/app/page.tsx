61234aa792647c1e274bf00214514d1500361507
devmcje <devmcje@gmail.com>
2024-10-13 16:19:03 +0800

    fix: use geist local font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,12 @@ export default function Home() {
     <main className="min-h-screen">
       <Button>Click me</Button>
       <ThemeModeToggle />
+
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus cum
+        non cupiditate id saepe minus enim. Sed fuga dolor, repellat a dolorem
+        enim optio, labore autem laborum accusantium, repellendus in.
+      </p>
     </main>
   );
 }
